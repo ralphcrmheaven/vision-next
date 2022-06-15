c2a602256b95bb6ffb2cb9cf37d090f685c0068c
Albert Gaya <albertgaya@ymail.com>
2022-06-15 19:06:38 +0800

    Add forgot password feature

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Base from './layout/Base'
 import FullScreen from './layout/FullScreen'
-import MeetingWrapper from './wrappers/MeetingWrapper';
+import MeetingWrapper from './wrappers/MeetingWrapper'
 import Conference from './pages/Conference'
 import SingleMeeting from './pages/SingleMeeting'
 import Home from './pages/Home'
@@ -13,6 +13,7 @@ import Signup from './pages/Signup'
 import Schedule from './pages/Schedule'
 import VerifyAccount from './pages/VerifyAccount'
 import ProtectedRoute from './ProtectedRoute'
+import ForgotPassword from './pages/ForgotPassword'
 
 const AppRoutes: React.FC = () => {
   return (
@@ -20,6 +21,7 @@ const AppRoutes: React.FC = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/verify-account" element={<VerifyAccount />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
 
       <Route
         path="/"
@@ -29,10 +31,24 @@ const AppRoutes: React.FC = () => {
           </ProtectedRoute>
         }
       >
-        <Route index element={<MeetingWrapper><Home /></MeetingWrapper>} />
+        <Route
+          index
+          element={
+            <MeetingWrapper>
+              <Home />
+            </MeetingWrapper>
+          }
+        />
         <Route path="/meetings" element={<Meetings />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/schedule" element={<MeetingWrapper><Schedule /></MeetingWrapper>} />
+        <Route
+          path="/schedule"
+          element={
+            <MeetingWrapper>
+              <Schedule />
+            </MeetingWrapper>
+          }
+        />
       </Route>
       <Route
         path="/"
