d9e1844c6ebb6a7f9ee1a12ef7443ee7c7ddb66a
Ashley Solomon <ashcslmn@gmail.com>
2022-07-14 15:54:59 +0800

    update aws config

--- src/Config.js
+++ src/Config.js
@@ -4,7 +4,7 @@
 const appConfig = {
   apiGatewayInvokeUrl:'',
   cognitoUserPoolId: 'us-east-1_hrThz9zsd',
-  cognitoAppClientId: '2ro2egj45r9d9ignah5tq3d6ms',
+  cognitoAppClientId: '7klejjc7831a32mdg0mnahqm1f',
   cognitoIdentityPoolId: 'us-east-1:791424a8-8e72-4361-aafd-e6f60ecc0f8d',
   appInstanceArn: 'arn:aws:chime:us-east-1:205131113421:app-instance/24db1c5e-2cb4-405b-8ece-0f4a22e3a340',
   region: 'us-east-1',  // Only supported region for Amazon Chime SDK Messaging as of this writing
