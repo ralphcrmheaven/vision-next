151cffe7e02590a725c5a9e37218774bd09b2df6
Ashley Solomon <ashcslmn@gmail.com>
2022-08-01 21:14:16 +0800

    update gitignore and utl crypt

--- src/utils/crypt.ts
+++ src/utils/crypt.ts
@@ -1,13 +1,13 @@
 import * as CryptoJS from 'crypto-js';
 
-//const secretKey = 'v!$!oNn3xt@20222022@v!$!oNn3xt!!';
+// const secretKey = 'v!$!oNn3xt@20222022@v!$!oNn3xt!!';
 const secretKey = process.env.REACT_APP_SK ?? '';
 const mode = CryptoJS.mode.CBC;
 const format = CryptoJS.format.Hex;
 
 export const decrypt = (encryptedText:any) => {
     const [ encryptedData, iv ] = encryptedText.split('|');
-  
+    console.log('secretKey:', secretKey)
     return CryptoJS.AES.decrypt(encryptedData, CryptoJS.enc.Utf8.parse(secretKey), {
       iv: CryptoJS.enc.Hex.parse(iv),
       mode: mode,
