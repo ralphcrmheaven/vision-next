d048dd069dd18ddf4f825e5508c18e72318bfb12
wreck it ralph <personal.ralphdegoma@gmail.com>
2022-09-08 20:33:34 +0800

    added woff to module exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,48 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-    extend: {
-      borderRadius: {
-        '6xl': '2.5rem',
-      },
-      colors: {
-        'vision-blue': '#1e3a8a',
-        'vision-light-blue': '#084D79',
-        'vision-lighter-blue': '#0B5A8C',
-        'vision-dark-blue': '#053F64',
-        'vision-yellow': '#FFC107',
-        'vision-light-yellow': '#FFC822',
-        'vision-lighter-yellow': '#FFD862',
-        'vision-sky': '#007AD9',
-        'vision-light-sky': '#1E8CE1',
-        'vision-lighter-sky': '#5AAFF1',
-        'vision-green': '#4CAF50',
-        'vision-light-green': '#58C05C',
-        'vision-lighter-green': '#64CF68',
-        'vision-cyan': '#008C8C',
-      },
-      scale: {
-        25: '0.25',
-        30: '0.30',
-      },
-      spacing: {
-        '220px': '220px',
-        '100px': '100px',
-        72.5: '18.5rem',
-        '2px': '2px',
-      },
-      width: {
-        455: '455px',
-        360: '360px',
-        972: '972px',
-      },
-      height: {
-        '310px': '310px',
-      },
-      padding: {
-        '20px': '20px',
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx,woff}', './public/index.html'],
+    theme: {
+        extend: {
+            borderRadius: {
+                '6xl': '2.5rem',
+            },
+            colors: {
+                'vision-blue': '#1e3a8a',
+                'vision-light-blue': '#084D79',
+                'vision-lighter-blue': '#0B5A8C',
+                'vision-dark-blue': '#053F64',
+                'vision-yellow': '#FFC107',
+                'vision-light-yellow': '#FFC822',
+                'vision-lighter-yellow': '#FFD862',
+                'vision-sky': '#007AD9',
+                'vision-light-sky': '#1E8CE1',
+                'vision-lighter-sky': '#5AAFF1',
+                'vision-green': '#4CAF50',
+                'vision-light-green': '#58C05C',
+                'vision-lighter-green': '#64CF68',
+                'vision-cyan': '#008C8C',
+            },
+            scale: {
+                25: '0.25',
+                30: '0.30',
+            },
+            spacing: {
+                '220px': '220px',
+                '100px': '100px',
+                72.5: '18.5rem',
+                '2px': '2px',
+            },
+            width: {
+                455: '455px',
+                360: '360px',
+                972: '972px',
+            },
+            height: {
+                '310px': '310px',
+            },
+            padding: {
+                '20px': '20px',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
