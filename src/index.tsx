85b8e386c3dc636c61a310992ff39ed85992bb0a
wreck it ralph <personal.ralphdegoma@gmail.com>
2022-10-08 01:42:07 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -35,6 +35,8 @@ window.addEventListener('load', () => {
   )
 })
 
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
