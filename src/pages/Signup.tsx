88e220a31042e68c4608094a2375ef75f1f499ef
Jerome Matias <jetbuquematias@gmail.com>
2022-12-08 11:35:25 +0800

    revision/auth

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -49,76 +49,81 @@ const Signup: React.FC = () => {
   }
 
   return (
-    <div className="mt-[11px] w-full sm:mt-14">
-      <div className="flex justify-center mb-[26px] sm:mb-10 h-[66px] sm:h-auto">
-        <Logo />
+    <div className="h-[100vh] flex justify-center auth-page">
+      <div className="mt-[11px] max-w-[757px] sm:mt-14 px-[171px]">
+        <div className="flex justify-center mb-[26px] sm:mb-10 h-[66px] sm:h-auto">
+          <Logo />
+        </div>
+
+        <div className="text-center">
+          <h1 className="mb-2 text-3xl font-bold text-vision-dark-blue text-[22px] sm:text-[30px]">
+            Signup for an Account
+          </h1>
+
+          <span className="text-[#747474] whitespace-pre sm:whitespace-normal">Start your 14 day trial. {"\n"}
+            No credit card account.</span>
+          <Form className="sign-up-mobile-view-form sm:sign-up-desktop-view-form" onSubmit={handleSubmit}>
+            {error && (
+              <Alert variation="error" className="mb-3 text-left">
+                {error}
+              </Alert>
+            )}
+            <div className="flex flex-row gap-2 w-full">
+              
+                <FormInput
+                  name="firstname"
+                  className="w-1/2 px-5 py-3 rounded-xl bg-slate-200"
+                  placeholder="First Name"
+                  onChange={(e: any) => setFirstname(e.target.value)}
+                />
+          
+                <FormInput
+                  name="lastname"
+                  className="w-1/2 px-5 py-3 rounded-xl bg-slate-200"
+                  placeholder="Last Name"
+                  onChange={(e: any) => setLastname(e.target.value)}
+                />
+              
+            </div>
+            <FormInput
+              type={InputTypes.Email}
+              name="email"
+              className="w-full px-5 py-3 rounded-xl bg-slate-200"
+              placeholder="Email Address"
+              onChange={(e: any) => setEmail(e.target.value)}
+              required
+            />
+
+            <FormInput
+              type={InputTypes.Password}
+              name="password"
+              className="w-full px-5 py-3  rounded-xl bg-slate-200"
+              placeholder="Password"
+              onChange={(e: any) => setPassword(e.target.value)}
+              required
+            />
+            <Button className='w-full' type={ButtonTypes.Submit} isLoading={isLoading}>
+              Signup
+            </Button>
+            <FormInput
+              type={InputTypes.Checkbox}
+              name="terms"
+              id="agree-to-terms"
+              className="w-full px-5 py-3  rounded-xl bg-slate-200"
+              placeholder="I agree to Terms and Conditions"              
+              required
+            />
+          </Form>
+
+          <span className="block mx-auto mb-3 text-[#747474] whitespace-pre sm:whitespace-normal">
+            Already have an{'\n'}account?{' '}
+            <Link to={'/login'} className="font-bold text-vision-dark-blue">
+              Login
+            </Link>
+          </span>          
+        </div>
       </div>
-
-      <div className="sign-up-mobile-view sm:sign-up-desktop-view">
-        <h1 className="mb-2 text-3xl font-bold text-vision-dark-blue text-[22px] sm:text-[30px]">
-          Signup for an Account
-        </h1>
-
-        <span className="text-[#747474] whitespace-pre sm:whitespace-normal">Start your 14 day trial. {"\n"}
-          No credit card account.</span>
-        <Form className="sign-up-mobile-view-form sm:sign-up-desktop-view-form" onSubmit={handleSubmit}>
-          {error && (
-            <Alert variation="error" className="mb-3 text-left">
-              {error}
-            </Alert>
-          )}
-          <div className="flex flex-row gap-2 w-full">
-            
-              <FormInput
-                name="firstname"
-                className="w-1/2 px-5 py-3 rounded-xl bg-slate-200"
-                placeholder="First Name"
-                onChange={(e: any) => setFirstname(e.target.value)}
-              />
-        
-              <FormInput
-                name="lastname"
-                className="w-1/2 px-5 py-3 rounded-xl bg-slate-200"
-                placeholder="Last Name"
-                onChange={(e: any) => setLastname(e.target.value)}
-              />
-            
-          </div>
-          <FormInput
-            type={InputTypes.Email}
-            name="email"
-            className="w-full px-5 py-3 rounded-xl bg-slate-200"
-            placeholder="Email Address"
-            onChange={(e: any) => setEmail(e.target.value)}
-            required
-          />
-
-          <FormInput
-            type={InputTypes.Password}
-            name="password"
-            className="w-full px-5 py-3  rounded-xl bg-slate-200"
-            placeholder="Password"
-            onChange={(e: any) => setPassword(e.target.value)}
-            required
-          />
-          <Button className='w-full' type={ButtonTypes.Submit} isLoading={isLoading}>
-            Signup
-          </Button>
-        </Form>
-
-        <span className="block mx-auto mb-3 text-[#747474] whitespace-pre sm:whitespace-normal">
-          Already have an{'\n'}account?{' '}
-          <Link to={'/login'} className="font-bold text-vision-dark-blue">
-            Login
-          </Link>
-        </span>
-
-        <img
-          src={cham3}
-          alt="Camera"
-          className="mobile-view-cham3-img sm:desktop-view-cham3-img"
-        />
-      </div>
+      <div className='w-full'></div>
     </div>
   )
 }
