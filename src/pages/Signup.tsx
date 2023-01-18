a79bfe20a3f7fc890ff8aa980a59c6516b0b1923
wreck it ralph <personal.ralphdegoma@gmail.com>
2023-01-18 20:26:38 +0800

    update redesign login signup verify confirmation

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -49,80 +49,92 @@ const Signup: React.FC = () => {
   }
 
   return (
-    <div className="h-[100vh] flex justify-center auth-page">
-      <div className="mt-[11px] max-w-[757px] sm:mt-14 px-[171px]">
-        <div className="flex justify-center mb-[26px] sm:mb-10 h-[66px] sm:h-auto">
-          <Logo />
-        </div>
+    <div className="auth-page">
+      <div className="auth-page__bg-signup"></div>
+      <div className="auth-page__login">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-11 lg:grid-cols-11">
+          <div className="col-span-1 sm:col-span-1 md:col-span-6 lg:col-span-6">
+            <div className="auth-page__form">
+              <div className="flex justify-center mb-[26px] sm:mb-10 h-[66px] sm:h-auto">
+                <Logo />
+              </div>
 
-        <div className="text-center">
-          <h1 className="mb-2 text-3xl font-bold text-vision-dark-blue text-[22px] sm:text-[30px]">
-            Signup for an Account
-          </h1>
+              <div className="text-center">
+                <h1 className="mb-2 text-3xl font-bold text-vision-dark-blue text-[22px] sm:text-[30px]">
+                  Signup for an Account
+                </h1>
 
-          <span className="text-[#747474] whitespace-pre sm:whitespace-normal">Start your 14 day trial. {"\n"}
-            No credit card account.</span>
-          <Form className="sign-up-mobile-view-form sm:sign-up-desktop-view-form" onSubmit={handleSubmit}>
-            {error && (
-              <Alert variation="error" className="mb-3 text-left">
-                {error}
-              </Alert>
-            )}
-            <div className="flex flex-row gap-2 w-full">
-              
-                <FormInput
-                  name="firstname"
-                  className="w-1/2 px-5 py-3 rounded-xl bg-slate-200"
-                  placeholder="First Name"
-                  onChange={(e: any) => setFirstname(e.target.value)}
-                />
-          
-                <FormInput
-                  name="lastname"
-                  className="w-1/2 px-5 py-3 rounded-xl bg-slate-200"
-                  placeholder="Last Name"
-                  onChange={(e: any) => setLastname(e.target.value)}
-                />
-              
+                <span className="text-[#747474] whitespace-pre sm:whitespace-normal">Start your 14 day trial. {"\n"}
+                  No credit card account.</span>
+                <Form className="sign-up-mobile-view-form w-full sm:w-full md:w-400 lg:w-455" onSubmit={handleSubmit}>
+                  {error && (
+                    <Alert variation="error" className="mb-3 text-left w-full sm:w-full md:w-400 lg:w-455">
+                      {error}
+                    </Alert>
+                  )}
+                  <div className="flex flex-row gap-2 w-full">
+                    
+                      <FormInput
+                        name="firstname"
+                        className="w-1/2 px-5 py-3 rounded-xl bg-[#ECECEC] placeholder-black"
+                        placeholder="First Name"
+                        onChange={(e: any) => setFirstname(e.target.value)}
+                      />
+                
+                      <FormInput
+                        name="lastname"
+                        className="w-1/2 px-5 py-3 rounded-xl bg-[#ECECEC] placeholder-black"
+                        placeholder="Last Name"
+                        onChange={(e: any) => setLastname(e.target.value)}
+                      />
+                    
+                  </div>
+                  <FormInput
+                    type={InputTypes.Email}
+                    name="email"
+                    className="w-full px-5 py-3 rounded-xl bg-[#ECECEC] placeholder-black"
+                    placeholder="Email Address"
+                    onChange={(e: any) => setEmail(e.target.value)}
+                    required
+                  />
+
+                  <FormInput
+                    type={InputTypes.Password}
+                    name="password"
+                    className="w-full px-5 py-3  rounded-xl bg-[#ECECEC] placeholder-black"
+                    placeholder="Password"
+                    onChange={(e: any) => setPassword(e.target.value)}
+                    required
+                  />
+                  <Button className='w-full' type={ButtonTypes.Submit} isLoading={isLoading}>
+                    Signup
+                  </Button>
+                  {/* <FormInput
+                    type={InputTypes.Checkbox}
+                    name="terms"
+                    id="agree-to-terms"
+                    className="w-full px-5 py-3  rounded-xl bg-slate-200"
+                    placeholder="I agree to Terms and Conditions"              
+                    required
+                  /> */}
+                </Form>
+
+                <span className="block mx-auto mb-3 text-[#747474] whitespace-pre sm:whitespace-normal">
+                  Already have an{'\n'}account?{' '}
+                  <Link to={'/login'} className="font-bold text-vision-dark-blue">
+                    Login
+                  </Link>
+                </span>          
+              </div>
             </div>
-            <FormInput
-              type={InputTypes.Email}
-              name="email"
-              className="w-full px-5 py-3 rounded-xl bg-slate-200"
-              placeholder="Email Address"
-              onChange={(e: any) => setEmail(e.target.value)}
-              required
-            />
-
-            <FormInput
-              type={InputTypes.Password}
-              name="password"
-              className="w-full px-5 py-3  rounded-xl bg-slate-200"
-              placeholder="Password"
-              onChange={(e: any) => setPassword(e.target.value)}
-              required
-            />
-            <Button className='w-full' type={ButtonTypes.Submit} isLoading={isLoading}>
-              Signup
-            </Button>
-            <FormInput
-              type={InputTypes.Checkbox}
-              name="terms"
-              id="agree-to-terms"
-              className="w-full px-5 py-3  rounded-xl bg-slate-200"
-              placeholder="I agree to Terms and Conditions"              
-              required
-            />
-          </Form>
-
-          <span className="block mx-auto mb-3 text-[#747474] whitespace-pre sm:whitespace-normal">
-            Already have an{'\n'}account?{' '}
-            <Link to={'/login'} className="font-bold text-vision-dark-blue">
-              Login
-            </Link>
-          </span>          
+          </div>
         </div>
       </div>
+
+      <img className="auth-page__plant--1" src="/images/plant_1.svg" alt="" />
+      <img className="auth-page__plant--2 invisible sm:invisible md:visible lg:visible " src="/images/plant_2.svg" alt="" />
+      <img className="auth-page__plant--3" src="/images/plant_3.svg" alt="" />
+
       <div className='w-full'></div>
     </div>
   )
--- src/pages/VerifyAccount.tsx
+++ src/pages/VerifyAccount.tsx
@@ -4,7 +4,7 @@ import { selectUser, confirmSignup } from '../redux/features/userSlice'
 import { Link, Navigate, useNavigate, useSearchParams } from 'react-router-dom'
 
 import Form from '../components/form/Form'
-import FormInput from '../components/form/FormInput'
+import FormInput, { InputTypes } from '../components/form/FormInput'
 import Button, { ButtonTypes } from '../components/Button'
 
 import Logo from '../components/Logo'
@@ -53,12 +53,12 @@ const VerifyAccount = () => {
 
   return (
     <>
-      <div className="mt-14">
+      <div className="mt-14 h-auto md:h-[700px] lg:h-[800px] overflow-hidden">
         <div className="flex justify-center mb-10">
           <Logo />
         </div>
 
-        <div className="relative mx-auto text-center bg-white w-972 rounded-3xl px-9 pt-14 pb-36">
+        <div className="relative mx-auto text-center bg-white w-full sm:w-full md:w-772 lg:w-972 rounded-3xl px-9 pt-14 pb-36">
           {isVerified ? (
             <>
               <h1 className="mb-2 text-3xl font-bold text-vision-dark-blue">
@@ -72,11 +72,17 @@ const VerifyAccount = () => {
 
               <Button
                 type={ButtonTypes.Submit}
-                className="w-455"
+                className="w-full sm:w-[400px] md:w-[400px] lg:w-455"
                 handleClick={handleClick}
               >
                 Login
               </Button>
+
+              <img
+                src="/images/verify_image.svg"
+                alt="Camera"
+                className="invisible sm:invisible md:visible lg:visible absolute w-[35%] -right-[130px] lg:-right-[170px] top-[20px]"
+              />
             </>
           ) : (
             <>
@@ -84,14 +90,14 @@ const VerifyAccount = () => {
                 Verify your account
               </h1>
 
-              <span>Please enter the code that we sent to your email.</span>
+              <span>Please enter your email address to verify your account.</span>
 
               <Form className="mt-10 mb-8" onSubmit={handleSubmit}>
                 {error && (
-                  <div className="inline-block mb-3 text-left w-455">
+                  <div className="inline-block mb-3 text-left w-full sm:w-[400px] md:w-[400px] lg:w-455">
                     <Alert
                       variation="error"
-                      className="mx-auto mb-3 text-left w-455"
+                      className="mx-auto mb-3 text-left w-full sm:w-[400px] md:w-[400px] lg:w-455"
                     >
                       {error}
                     </Alert>
@@ -99,9 +105,10 @@ const VerifyAccount = () => {
                 )}
 
                 <FormInput
-                  name="code"
-                  className="px-5 py-3 mb-3 text-center rounded-xl bg-slate-200 w-455"
-                  placeholder="Code"
+                  name="Email address"
+                  type={InputTypes.Email}
+                  className="px-5 py-3 mb-3 rounded-xl bg-[#ECECEC] placeholder-black w-full sm:w-[400px] md:w-[400px] lg:w-455"
+                  placeholder="Email address"
                   onChange={(e: any) => setCode(e.target.value)}
                   required
                 />
@@ -109,7 +116,7 @@ const VerifyAccount = () => {
                 <Button
                   type={ButtonTypes.Submit}
                   isLoading={isLoading}
-                  className="w-455"
+                  className="w-full sm:w-[400px] md:w-[400px] lg:w-455"
                 >
                   Verify Account
                 </Button>
@@ -123,9 +130,9 @@ const VerifyAccount = () => {
               </span>
 
               <img
-                src={cham3}
+                src="/images/verify_image.svg"
                 alt="Camera"
-                className="absolute -left-220px top-100px"
+                className="invisible sm:invisible md:visible lg:visible absolute w-[35%] -right-[130px] lg:-right-[170px] top-[100px]"
               />
             </>
           )}
