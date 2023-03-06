f57b2748b7ee175ba1cf73c901ac98ef8cef748d
wreck it ralph <personal.ralphdegoma@gmail.com>
2023-03-06 22:06:01 +0800

    changed config appinstance

--- src/Config.js
+++ src/Config.js
@@ -6,7 +6,7 @@ const appConfig = {
     cognitoUserPoolId: 'us-east-1_hrThz9zsd',
     cognitoAppClientId: '7klejjc7831a32mdg0mnahqm1f',
     cognitoIdentityPoolId: 'us-east-1:791424a8-8e72-4361-aafd-e6f60ecc0f8d',
-    appInstanceArn: 'arn:aws:chime:us-east-1:205131113421:app-instance/24db1c5e-2cb4-405b-8ece-0f4a22e3a340',
+    appInstanceArn: 'arn:aws:chime:us-east-1:205131113421:app-instance/89e0b5e9-bf46-4919-93a9-3c03ca877e51',
     region: 'us-east-1', // Only supported region for Amazon Chime SDK Messaging as of this writing
     attachments_s3_bucket_name: 'amplify-visionnext-dev-2241-chatattachmentsbucket-wiytu3g1sei0'
 };
